68eb12b9df3f206a016bb117ead282b871bfe6c5
liuzijian <liuzijian@m-tour.com.cn>
2019-08-30 19:42:18 +0800

    sentinel

--- spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/example/client/UserClient.java
+++ spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/example/client/UserClient.java
@@ -1,12 +1,13 @@
 package com.example.client;
 
+import com.example.client.fallback.UserFallback;
 import com.example.dto.UserReq;
 import com.example.dto.UserResp;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("cloud-provider")
+@FeignClient(value = "cloud-provider", fallback = UserFallback.class)
 public interface UserClient {
     @RequestMapping("/getUser")
     UserResp getUser(@RequestBody UserReq req);
