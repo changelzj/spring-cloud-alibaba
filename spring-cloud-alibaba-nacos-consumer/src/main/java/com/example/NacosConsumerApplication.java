0c27ddaf5e5d7a39c819ff7ac95f043d80237cb7
liuzijian <liuzijian@m-tour.com.cn>
2019-09-30 14:07:02 +0800

    update

--- spring-cloud-alibaba-nacos-consumer/src/main/java/com/example/NacosConsumerApplication.java
+++ spring-cloud-alibaba-nacos-consumer/src/main/java/com/example/NacosConsumerApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class NacosConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosConsumerApplication.class, args);
