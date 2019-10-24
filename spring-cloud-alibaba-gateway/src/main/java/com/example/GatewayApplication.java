2ffd23c42660037a851165d44d3f746d9fc08d3e
liuzijian <liuzijian@m-tour.com.cn>
2019-10-24 20:06:09 +0800

    add gateway

--- spring-cloud-alibaba-gateway/src/main/java/com/example/GatewayApplication.java
+++ spring-cloud-alibaba-gateway/src/main/java/com/example/GatewayApplication.java
@@ -3,13 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
-public class NacosProviderApplication {
+@EnableFeignClients
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NacosProviderApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
